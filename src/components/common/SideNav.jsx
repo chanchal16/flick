c49a2300c69b4a09be1bb375b91bd3c84c778aa8
chanchal16 <rajputchanchal17@gmail.com>
2022-04-19 13:22:28 +0530

    chore - add navlinks

--- src/components/common/SideNav.jsx
+++ src/components/common/SideNav.jsx
@@ -1,35 +1,39 @@
 import React from 'react';
 import '../../styles/sidenav.css';
 import {MdOutlineHome,MdOutlineThumbUp,MdPlaylistAdd,MdOutlineExplore,MdOutlineWatchLater,MdHistory} 
-from 'react-icons/md'
+from 'react-icons/md';
+import { Link, NavLink } from "react-router-dom";
 
 export function SideNav() {
+    const getClassName = ({isActive})=>{
+        return isActive ? "nav-link active" : "nav-link"        
+    }
   return (
     <div className='sidenav'>
-        <div className='nav-link'>
+        <NavLink to={'/'} className={getClassName}>
             <MdOutlineHome size='1.5em'/>
             <span className='text-xs'>Home</span>
-        </div>
-        <div className='nav-link'>
+        </NavLink>
+        <NavLink to={'/explore'} className={getClassName}>
             <MdOutlineExplore size='1.5em'/>
             <span className='text-xs'>explore</span>
-        </div>
-        <div className='nav-link'>
+        </NavLink>
+        <NavLink to={'/liked'} className={getClassName}>
             <MdOutlineThumbUp size='1.5em'/>
             <span className='text-xs'>Liked</span>
-        </div>
-        <div className='nav-link'>
+        </NavLink>
+        <NavLink to={'/playlist'} className={getClassName}>
             <MdPlaylistAdd size='1.5em'/>
             <span className='text-xs'>Playlist</span>
-        </div>
-        <div className='nav-link'>
+        </NavLink>
+        <NavLink to={'/watchlater'} className={getClassName}>
             <MdOutlineWatchLater size='1.5em'/>
             <span className='text-xs'>Watch later</span>
-        </div>
-        <div className='nav-link'>
+        </NavLink>
+        <NavLink to={'/history'} className={getClassName}>
             <MdHistory size='1.5em'/>
             <span className='text-xs'>History</span>
-        </div>
+        </NavLink>
     </div>
   )
 }
