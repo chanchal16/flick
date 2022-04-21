a21aea92a32ec84a1ab92e8ec06dae2d59b815d4
chanchal16 <rajputchanchal17@gmail.com>
2022-04-21 13:13:35 +0530

    chore - named exports pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 export {Login} from './Login';
 export {Signup} from './Signup'
 export {VideoListing} from './VideoListing';
-export {WatchLater} from './WatchLater'
+export {WatchLater} from './WatchLater';
+export {Playlists} from './Playlists';
+export {SinglePlaylist} from './SinglePlaylist';
