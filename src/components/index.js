baa66b1edfa843f1a3431b18e77ed31b9b6acb5a
chanchal16 <rajputchanchal17@gmail.com>
2022-04-21 13:13:55 +0530

    chore - named exports components

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,5 @@
 export {VideoCard} from './VideoCard';
 export {PlaylistVideoCard} from './PlaylistVideoCard'
+export {PlaylistCard} from './PlaylistCard';
+export {PlaylistModal} from './PlaylistModal'
+
