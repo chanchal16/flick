401f44b0b7f5ebe0431b00a0aae6ccfe67434687
chanchal16 <rajputchanchal17@gmail.com>
2022-04-21 10:51:57 +0530

    refactor - add card component

--- src/pages/WatchLater.jsx
+++ src/pages/WatchLater.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { usePlaylist,useAuth } from '../contexts/MainProvider';
-import {MdDelete} from 'react-icons/md';
+import { usePlaylist} from '../contexts/MainProvider';
 import {removeFromWatchLater} from '../services/watchLater-services'
+import { PlaylistVideoCard } from '../components';
 
 function WatchLater() {
-    const {token} = useAuth()
     const {playListState,playListDispatch} = usePlaylist();
     const{watchlater} = playListState
   return (
@@ -22,26 +21,7 @@ function WatchLater() {
         <div className='list-container'>           
             {
                 watchlater.map(video=>(
-                    <div class="horizontal-card">
-                        <div class="card-media">
-                            <img class="hc-image res-image"
-                            src={`https://img.youtube.com/vi/${video._id}/mqdefault.jpg`} 
-                            alt="video" />                
-                        </div>
-                        <div class="content">
-                            <h3 class="card-title">{video.title}</h3>
-                            <p class="gray card-sub-title">{video.creator}</p>
-                            <div className='content-footer'>
-                                <div className='info'>
-                                <span className='gray'>{video.views} views</span>
-                                <span className='gray'>{video.published} years ago</span>
-                                </div>
-                                <span className='delete' onClick={()=>removeFromWatchLater(token,playListDispatch,video._id)}>
-                                    <MdDelete size='1.5em'/>
-                                </span>
-                            </div>
-                        </div>                        
-                    </div>
+                    <PlaylistVideoCard key={video._id} video={video} deleteVideo={removeFromWatchLater} />
                 ))
             }
         </div>
