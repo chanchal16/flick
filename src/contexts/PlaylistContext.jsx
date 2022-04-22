b75fe6a7374cc49d90c43837c27e0d911d762d36
chanchal16 <rajputchanchal17@gmail.com>
2022-04-22 13:11:19 +0530

    chore - add playlists state

--- src/contexts/PlaylistContext.jsx
+++ src/contexts/PlaylistContext.jsx
@@ -1,23 +1,30 @@
-import React,{createContext,useContext,useReducer,useEffect}from 'react'
+import React,{createContext,useContext,useReducer,useEffect,useState}from 'react'
 import { playlistReducer } from '../reducers/playlist-reducer'
+import { getAllPlaylists, getPlaylist } from '../services/playlist-services'
 import { getVideosFromWatchLater } from '../services/watchLater-services'
 import { useAuth } from './AuthContext'
 
 const playlistContext = createContext()
 const initialState = {
-    watchlater:[]
+    watchlater:[],
+    playlists:[]
 }
  function PlaylistContextProvider({children}) {
+   const [isModalOpen,setIsModalOpen] = useState(false)
+   const[selectedVideo,setSelectedVideo] = useState(null)
     const {token} = useAuth()
     const [playListState,playListDispatch] = useReducer(playlistReducer,initialState)
 
     useEffect(() => {
+      getAllPlaylists(token,playListDispatch)
       getVideosFromWatchLater(token,playListDispatch)      
     }, [])
+
+    const providerObj = {playListState,playListDispatch,isModalOpen,setIsModalOpen,selectedVideo,setSelectedVideo}
     
   return (
     <div>
-        <playlistContext.Provider value={{playListState,playListDispatch}}>
+        <playlistContext.Provider value={providerObj}>
             {children}
         </playlistContext.Provider>
     </div>
