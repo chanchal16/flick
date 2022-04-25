8ee71c5d13f81b6865ea8baa40e4c97b8f2ec89c
chanchal16 <rajputchanchal17@gmail.com>
2022-04-25 22:20:01 +0530

    chore - export liked page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,3 +5,4 @@ export {WatchLater} from './WatchLater';
 export {Playlists} from './Playlists';
 export {SinglePlaylist} from './SinglePlaylist';
 export {Video} from './Video';
+export {Liked} from './Liked'
