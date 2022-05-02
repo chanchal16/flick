31645c4bc7552e1435589bd3792f5db6bab0de7b
chanchal16 <rajputchanchal17@gmail.com>
2022-05-02 22:51:04 +0530

    chore - export category card component

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,5 @@ export {PlaylistVideoCard} from './PlaylistVideoCard'
 export {PlaylistCard} from './PlaylistCard';
 export {PlaylistModal} from './PlaylistModal';
 export {VideoPlayer} from './VideoPlayer';
-export {SimilarVideoCard} from './SimilarVideoCard'
-
+export {SimilarVideoCard} from './SimilarVideoCard';
+export {CategoryCard} from './CategoryCard';
