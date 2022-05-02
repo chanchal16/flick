1889600eaeb4bf6a7d07c356dfe1a69cdfae8a98
chanchal16 <rajputchanchal17@gmail.com>
2022-05-02 22:52:52 +0530

    Create reducer for videos

---
+++ src/reducers/video-reducer.js
@@ -0,0 +1,14 @@
+const videoReducer = (state,{type,payload})=>{
+    switch(type){
+        case 'LOAD_VIDEOS':
+            return {...state,videos:payload}
+        case 'FILTER_BY_CATEGORY':
+            return {
+                ...state,
+                category:payload
+            }
+        default:
+            return state;
+    }
+}
+export {videoReducer}
