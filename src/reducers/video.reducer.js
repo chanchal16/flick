022f4985ed9c4d257aced37f2d9d45122f6354ec
chanchal16 <rajputchanchal17@gmail.com>
2022-05-02 22:53:01 +0530

    Delete video.reducer.js

--- src/reducers/video.reducer.js
+++
@@ -1,9 +0,0 @@
-const videoReducer = (state,{type,payload})=>{
-    switch(type){
-        case 'LOAD_VIDEOS':
-            return {...state,videos:payload}
-        case 'FILTER_BY_CATEGORY':
-            return {...state,category:payload}
-    }
-}
-export {videoReducer}
