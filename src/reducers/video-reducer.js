83a042cbcf3fa7cbcd44bbe66eaf0d6bf6b7660e
chanchal16 <rajputchanchal17@gmail.com>
2022-05-06 23:26:33 +0530

    Update - add search case

--- src/reducers/video-reducer.js
+++ src/reducers/video-reducer.js
@@ -3,10 +3,9 @@ const videoReducer = (state,{type,payload})=>{
         case 'LOAD_VIDEOS':
             return {...state,videos:payload}
         case 'FILTER_BY_CATEGORY':
-            return {
-                ...state,
-                category:payload
-            }
+            return {...state, category:payload}
+        case 'SEARCH':
+            return {...state, searchQuery:payload};
         default:
             return state;
     }
