3197cb2949a7ab415fa270ebd7b5dc1d18d4523b
chanchal16 <rajputchanchal17@gmail.com>
2022-05-06 23:25:19 +0530

    Update - add search bar

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import {Link,useNavigate} from 'react-router-dom'
 import '../../styles/nav.css';
 import logo from '../../assets/shiny-iris.svg'
+import { useVideo } from '../../contexts/VideoContext';
+import {MdSearch} from 'react-icons/md'
 
 export function Navbar() {
+  const{videoState,videoDispatch} = useVideo()
+  const navigate = useNavigate()
+
+  const handleSearch = (e)=>{
+    e.preventDefault()
+    navigate('/videos')   
+  }
   return (
         <header className="navbars">
             <figure className='menu-icon'>
@@ -13,6 +22,11 @@ export function Navbar() {
               flick            
             </a>
             <nav>
+              <form onSubmit={handleSearch} className='search-form'>
+                <input type='search' className='search' placeholder='search'
+                value={videoState.searchQuery} onChange={(e)=>videoDispatch({type:'SEARCH',payload:e.target.value.toLowerCase()})} />
+                <span className='search-icon gray'><MdSearch size='1rem'/> </span>
+              </form>
               <Link to='/login'> 
                 <button className="login-btn">
                   Login
