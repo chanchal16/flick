a91fc39b2fff3d249701d089ea6a2199d9c55c68
chanchal16 <rajputchanchal17@gmail.com>
2022-05-07 15:12:51 +0530

    Create routes.js

---
+++ src/routes.js
@@ -0,0 +1,56 @@
+import {Liked,Home,History,Login,Playlists,Signup,SinglePlaylist,Video,VideoListing, WatchLater } from "./pages";
+import {RequireAuth,PageNotFound} from './components/common/'
+
+const ROUTES = [
+    {
+        path:"/",
+        element:<Home/>
+    },
+    {
+        path:"videos",
+        element:<VideoListing/>
+    },
+    {
+        path:"/:videoId",
+        element:<Video/>
+    },
+    {
+        path:"/login",
+        element:<Login />
+    },
+    {
+        path:"signup",
+        element:<Signup/>
+    },
+    {
+        path:'/',
+        element:<RequireAuth/>,
+        children:[
+            {
+                path:"watchlater",
+                element:<WatchLater/>
+            },
+            {
+                path:"playlists",
+                element:<Playlists/>
+            },
+            {
+                path:"playlists/:playlistId",
+                element:<SinglePlaylist/>
+            },
+            {
+                path:"liked",
+                element:<Liked/>
+            },
+            {
+                path:"history",
+                element:<History/>
+            }
+        ]
+    },
+    {
+        path: "*",
+        element: <PageNotFound />,
+    },
+]
+export {ROUTES}
