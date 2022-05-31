fa326c2bd718fdeeee6cb62a1e5ab0b48cc0cc4d
chanchal16 <rajputchanchal17@gmail.com>
2022-05-31 11:18:27 +0530

    Create debounce function

---
+++ src/Utils/debounce.js
@@ -0,0 +1,11 @@
+const debounce = function(fn,d){
+    let timer;
+    return function(){
+        let context = this, args = arguments;
+        clearTimeout(timer);
+        timer = setTimeout(()=>{
+            fn.apply(context,args)
+        },d);
+    }
+}
+export{debounce}
