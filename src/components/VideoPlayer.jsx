3061318e0292270f83162ac4ed0c75e852523c64
chanchal16 <rajputchanchal17@gmail.com>
2022-06-01 12:38:11 +0530

    Update - add query to url

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -5,7 +5,7 @@ export function VideoPlayer({video}) {
 
         <iframe width="700"
          height="361" 
-         src={`https://www.youtube.com/embed/${video._id}`} 
+         src={`https://www.youtube.com/embed/${video._id}?autoplay=1`} 
          title="YouTube video player" 
          frameBorder="0" 
          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
