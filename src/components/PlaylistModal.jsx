a3a909d055b05104e478bfe999d5f58ed282c279
chanchal16 <rajputchanchal17@gmail.com>
2022-06-01 11:26:27 +0530

    Update- add modal

--- src/components/PlaylistModal.jsx
+++ src/components/PlaylistModal.jsx
@@ -3,9 +3,10 @@ import { usePlaylist,useAuth } from '../contexts/MainProvider'
 import { addVideoToPlaylist, createPlaylist, removeVideoFromPlaylist } from '../services/playlist-services'
 import { checkIfExists } from '../Utils/check-if-exists';
 import {MdClose} from 'react-icons/md'
+import { Link } from 'react-router-dom';
 
 export function PlaylistModal() {
-    const {token} = useAuth()
+    const {token,user} = useAuth()
     const[playlistTitle,setPlaylistTitle] = useState('')
     const{isModalOpen,setIsModalOpen,playListDispatch,playListState,selectedVideo} = usePlaylist()
     const{playlists} = playListState
@@ -26,8 +27,10 @@ export function PlaylistModal() {
     isModalOpen && (
     <div className='playlist-modal'>
       <div className='dialogbox'>
+        {user ? (
+          <>
         <div className='dialog-header'>
-          <h4>add to..</h4>
+          <h4 className='text-sm'>add to..</h4>
           <span className='close' onClick={()=>setIsModalOpen(false)}><MdClose/> </span>
         </div>
         
@@ -35,25 +38,38 @@ export function PlaylistModal() {
           <input type='text' placeholder='enter name' className='input-field'
            value={playlistTitle} onChange={(e)=>setPlaylistTitle(e.target.value)}/>
            <div className='dialog-action'>
-          <button className='action-btns add' >Create</button>
-          <button className='action-btns cancel' onClick={()=>setIsModalOpen(false)}>Cancel</button>
+            <button className='action-btns add' >Create</button>
+            <button className='action-btns cancel' onClick={()=>setIsModalOpen(false)}>Cancel</button>
           </div>
         </form>
         {
-          playlists.length > 0 && <div className='playlists'>
+          playlists.length > 0 && 
+          <div >
           {
             playlists?.map(playlist=>(
-              <div key={playlist._id}>
+              <div key={playlist._id} className='playlists'>
                 <label className='text-sm accent'>
                 <input type ='checkbox' checked={!!checkIfExists(playlist.videos,selectedVideo._id)}
                   onChange={()=>handlePlaylistHandler(playlist)}/>
-                 {playlist.title}          
+                 &nbsp; {playlist.title}          
                 </label>
               </div>              
             ))
           }
           </div>
-        }
+          
+        }</>) :(
+          <div className='d-flex'>
+            <div className='dialog-header'>
+              <h4 className='text-md bold'>You are not logged in. Please login</h4>
+              <span className='close' onClick={()=>setIsModalOpen(false)}><MdClose/> </span>
+            </div>
+            <div >
+              <Link to='/login'><button className='button primary-btn' onClick={()=>setIsModalOpen(false)}>Login</button></Link>
+              <Link to='/signup'><button className='button secondary-btn'onClick={()=>setIsModalOpen(false)}>Signup</button></Link>
+            </div>
+          </div>
+        )}
       </div>
     </div>
     )
