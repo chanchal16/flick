ad100c463db218a917448000481487fbf7e091ef
chanchal16 <rajputchanchal17@gmail.com>
2022-06-02 10:19:36 +0530

    Update - add toast container

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
 import { Navbar,RequireAuth,SideNav } from "./components/common";
-import { Routes, Route,useRoutes } from "react-router-dom";
+import {useRoutes } from "react-router-dom";
 import {PlaylistModal} from "./components";
 import { ROUTES } from "./routes";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const routeElement = useRoutes(ROUTES)
   return (
     <div className="App">
       <Navbar/>
+      <ToastContainer autoClose={1000} pauseOnFocusLoss={false} />
       <main className="app-main">
         <SideNav/>
         <div className="app-content">
